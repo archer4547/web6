5509c147661bd91e4d00ab563d593a0355d2e77c
archer4547 <114151588+archer4547@users.noreply.github.com>
2022-12-19 11:30:38 +0200

    Add files via upload

---
+++ comments/index_post.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+    $message = $_POST['comment'];
+
+    $fp = fopen("index_comments.php", 'a');
+
+    if (!$fp)
+    {
+        echo 'Error!';
+        exit;
+    }
+    else if (isset($_SESSION['user']))
+    {
+        $output = "Name: " . $_SESSION['user'][0] . '<br>Comment: <br>' . $message . "<br><br>\n";
+        fwrite ($fp, $output, strlen($output));
+        fclose($fp);
+        header("Location: ../index.php");
+        exit;
+    }
+    else if (!isset($_SESSION['user']))
+    {
+        echo 'Login first!';
+    }
---
+++ comments/not_index_comments.php
@@ -0,0 +1,2 @@
+Name: name<br>Comment: <br>another comment<br><br>
+Name: Michael<br>Comment: <br>this is the 4th comment<br><br>
